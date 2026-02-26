8624067f52aa70a597d6b1e2b8b4bf7d270c71e1
User <user@example.com>
2026-02-26 19:54:18 +0900

    fix(db): add missing Inspector import to migration

--- backend/alembic/versions/e4c210e9598e_add_attachment_file_to_quality_defects.py
+++ backend/alembic/versions/e4c210e9598e_add_attachment_file_to_quality_defects.py
@@ -9,6 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.engine.reflection import Inspector
 
 
 # revision identifiers, used by Alembic.
