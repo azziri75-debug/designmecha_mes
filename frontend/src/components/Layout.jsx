b63681855ed15c788948164cc5ac0848fbe61b8d
User <user@example.com>
2026-03-30 10:43:40 +0900

    fix: Layout.jsx api 모듈 Import 경로 지정 오류 수정 (../lib/api)

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Outlet, Link, useLocation, useNavigate } from 'react-router-dom';
-import api from '../api';
+import api from '../lib/api';
 import {
     LayoutDashboard,
     Package,
